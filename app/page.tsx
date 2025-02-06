f03648371d9806a05e9e976f54ac435e7c25a9d9
Jordan Roberts <jrobertswebdev@gmail.com>
2025-02-05 23:29:36 -0600

    remove mapbox

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,8 @@
-import AppMap from '@/components/app-map'
 export default function Home() {
   return (
     <div className="flex flex-col items-center justify-center h-screen">
       <main className="flex flex-col items-center justify-center">
         <h1 className="text-4xl font-bold font-sans">HEYo</h1>
-        <AppMap />
       </main>
     </div>
   );
